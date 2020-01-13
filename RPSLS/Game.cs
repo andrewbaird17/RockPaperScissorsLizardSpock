4bf9418f79803084613879f4a3bc3c06d0fdb490
Andrew Baird <baird.andrew4@gmail.com>
2020-01-13 10:56:10 -0600

    basic game instructions display when game starts

--- RPSLS/Game.cs
+++ RPSLS/Game.cs
@@ -19,10 +19,28 @@ namespace RPSLS
         //Member Methods (CAN DO)
         public void RunGame()
         {
-
+            GameInstructions();
+            string choice = ChooseGamemode();
         }
 
-        public void ChooseGamemode()
+        public void GameInstructions()
+        {
+            Console.WriteLine("Welcome! You have chosen to play a game of Rock, Paper, Scissors, Lizard, Spock \nThe goal of the game is to beat a computer or another player over multiple rounds. \nA winner is declarecd once one of the players has successfully won three rounds.");
+            Console.WriteLine("Please select a gamemode to begin!");
+            Console.ReadLine();
+        }
+        public string ChooseGamemode()
+        {
+            string choice;
+            do
+            {
+                Console.WriteLine("Pick an option:\n1) Human\n2) Computer");
+                choice = Console.ReadLine();
+            } while (choice != "1" && choice != "2");
+            return choice;
+        }
+
+        public void BadInput()
         {
 
         }
